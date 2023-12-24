da3c17ca243a352f030983aeed96b6c2749a3a0f
HyprHex <swapapis@gmail.com>
2023-12-24 20:32:04 +0100

    Update a record and return how many row affected

--- main.go
+++ main.go
@@ -42,16 +42,35 @@ func main() {
 
 	fmt.Println("Successfully connected")
 
+	// Insert record 
+
+	// sqlStatement := `
+	// INSERT INTO users (age, email, first_name, last_name)
+	// VALUES ($1, $2, $3, $4)
+	// RETURNING id`
+	// id := 0
+	// err = db.QueryRow(sqlStatement, 29, "father@gmai.com", "Father", "Seyf").Scan(&id)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	//
+	// fmt.Println("New record ID is:", id)
+	//
+
+	// Update record 
 	sqlStatement := `
-	INSERT INTO users (age, email, first_name, last_name)
-	VALUES ($1, $2, $3, $4)
-	RETURNING id`
-	id := 0
-	err = db.QueryRow(sqlStatement, 29, "father@gmai.com", "Father", "Seyf").Scan(&id)
+	UPDATE users SET first_name = $2, last_name = $3
+	WHERE id = $1`
+	res, err := db.Exec(sqlStatement, 3, "NewFirst", "NewLast")
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println("New record ID is:", id)
+	count, err := res.RowsAffected()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(count)
 
 }
