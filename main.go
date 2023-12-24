ed4ce728d9e89feddad6b9d1419b24095ad31b3b
HyprHex <swapapis@gmail.com>
2023-12-24 19:24:47 +0100

    Add env variable for Database information

--- main.go
+++ main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
 
+	"github.com/joho/godotenv"
+)
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	var (
+		host   = os.Getenv("HOST")
+		port   = os.Getenv("PORT")
+		user   = os.Getenv("USERNAME")
+		pass   = os.Getenv("PASSWORD")
+		dbname = os.Getenv("DBNAME")
+	)
+
+	fmt.Println(host, port, user, pass, dbname)
 }
