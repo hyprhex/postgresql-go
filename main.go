27b4d8e45afb5237c6d1c877f885a73178e9654c
HyprHex <swapapis@gmail.com>
2023-12-24 19:41:34 +0100

    Create a connection to DB

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"os"
 
 	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 func init() {
@@ -23,5 +25,21 @@ func main() {
 		dbname = os.Getenv("DBNAME")
 	)
 
-	fmt.Println(host, port, user, pass, dbname)
+	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s "+"password=%s dbname=%s sslmode=disable",
+		host, port, user, pass, dbname)
+
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Successfully connected")
+
 }
